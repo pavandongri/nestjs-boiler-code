b87590a1cb42392cb438e558d2477297e29b41dd
Dongri Pavan Kumar <pavankumardongri@gmail.com>
2026-03-03 16:02:17 +0530

    added eslint, prettier and husky

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { AppLogger } from './core/logger/logger.service';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { AppLogger } from "./core/logger/logger.service";
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly config: ConfigService,
-    private readonly logger: AppLogger,
+    private readonly logger: AppLogger
   ) {}
 
   getHello(): object {
-    const dbUrl = this.config.get('DB_URL');
+    const DB_URL = this.config.get("DB_URL");
+    const BASE_URL = this.config.get("BASE_URL");
 
-    this.logger.log(`DB_URL: ${dbUrl}`);
+    this.logger.log(`DB_URL: ${DB_URL}`);
+    this.logger.log(`BASE_URL: ${BASE_URL}`);
 
-    return { message: 'Hello World!' };
+    return { message: "Hello World!" };
   }
 }
