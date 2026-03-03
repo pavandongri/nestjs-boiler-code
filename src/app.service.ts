270d27fc58cb0ecde8280ec2c46fbcf227055fd9
Dongri Pavan Kumar <pavankumardongri@gmail.com>
2026-03-03 18:16:32 +0530

    changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { AppLogger } from "./core/logger/logger.service";
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly config: ConfigService,
-    private readonly logger: AppLogger
-  ) {}
+  constructor() {}
 
   getHello(): object {
     return { message: "Hello World!" };
