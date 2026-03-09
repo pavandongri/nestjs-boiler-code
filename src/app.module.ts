f4c5acd40613b181285974237167cc1819a6d6da
bhavna-datanova <bhavna@godatanova.com>
2026-03-09 16:06:19 +0530

    modify folders and naming files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoreModule } from "./core/core.module";
-import { DatabaseModule } from "./core/db/database.module";
-import { ProviderModule } from "./modules/providers/provider.module";
 
 @Module({
-  imports: [CoreModule, DatabaseModule, ProviderModule],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService]
 })
