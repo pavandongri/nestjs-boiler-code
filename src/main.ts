b78b569d13046481b34ff0cab0303502764eaad2
Dongri Pavan Kumar <pavankumardongri@gmail.com>
2026-03-17 00:46:09 +0530

    added db warm up

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 import { APP_CONSTANTS } from "./constants/app.contants";
+import { db } from "./core/db";
 import { AppLogger } from "./core/logger/logger.service";
 
 async function main() {
@@ -17,6 +18,14 @@ async function main() {
   const port = config.get<number>("PORT") ?? 3000;
 
   app.setGlobalPrefix(APP_CONSTANTS.API_PREFIX);
+
+  try {
+    await db.execute("SELECT 1");
+    logger.log("✅ Database connected successfully");
+  } catch (err) {
+    logger.error("❌ Database connection failed", err);
+    process.exit(1);
+  }
 
   await app.listen(port);
 
